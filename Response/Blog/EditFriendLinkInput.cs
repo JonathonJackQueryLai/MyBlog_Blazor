985d2bd4389776779696d39de6857e843a0e993a
田 朱 <1033566918@qq.com>
2020-06-28 16:37:20 +0800

    添加角色管理

---
+++ Response/Blog/EditFriendLinkInput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Acme.BookStore.Application.Contracts.Blog
+{
+    public class EditFriendLinkInput : FriendLinkDto
+    {
+    }
+}
